1945ae40d6214da399dd0be551d103e0bd04767b
unknown <jp@.(none)>
2011-02-19 22:14:23 +0300

    1

--- app/models/debit_category.rb
+++ app/models/debit_category.rb
@@ -1,8 +1,12 @@
 class DebitCategory < ActiveRecord::Base
-attr_accessible :category
+	attr_accessible :category
 
-has_many :debits
+	has_many :debits
 
-validates_presence_of :category
+	validates_presence_of :category
+
+	#Named Scopes
+	#orders debits by debit_id asscending 
+	named_scope :all, :order => "id ASC"
 
 end
