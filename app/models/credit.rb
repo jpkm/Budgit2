fb3d3f5ac09bcbb228258b4c8d97542acb078309
jpmenniti <jpkm1989@gmail.com>
2011-02-20 20:14:06 +0300

    7

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -4,6 +4,6 @@ class Credit < ActiveRecord::Base
 	belongs_to :credit_category
 	belongs_to :account
 	
-	validates_numericality_of :category_id, :date, :account_id, :amount
+	validates_numericality_of :credit_category_id, :date, :account_id, :amount
 	
 end
