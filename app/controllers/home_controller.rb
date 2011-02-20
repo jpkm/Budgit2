98569e8e75adfd52ed2bb230f268db238832ce2f
jpmenniti <jpkm1989@gmail.com>
2011-02-20 20:55:14 +0300

    8

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+  
+  # list of assignments for the user
+  @assignment_for_user = Assignment.for_user(current_user.id).all
+  
+  
   end
 
 end
