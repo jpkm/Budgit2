e0d81599e8f3e97db1fb81e425fe553a1b9a407e
jpmenniti <jpkm1989@gmail.com>
2011-02-22 10:02:00 +0300

    2

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,9 +1,17 @@
 class Credit < ActiveRecord::Base
 	attr_accessible :credit_category_id, :date, :account_id, :amount
 
-	belongs_to :credit_category
+	# Relationships
+	belongs_to :creditcategory
 	belongs_to :account
 	
+	# Validations
 	validates_numericality_of :credit_category_id, :date, :account_id, :amount
+	
+	#Named Scopes
+	#orders debits by debit_id asscending 
+    named_scope :all, :order => "id ASC"
+    # get all the credits by a particular account
+    named_scope :for_account, lambda { |account_id| { :conditions => ['account_id = ?', account_id] } }
 	
 end
