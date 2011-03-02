94b234bf4d93e6cb0d0bcde1b5041ceb3be0bdbe
jpmenniti <jpkm1989@gmail.com>
2011-03-02 17:00:23 +0200

    6

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(:version => 20110219172523) do
 
   create_table "credits", :force => true do |t|
     t.integer  "credit_category_id"
-    t.datetime "date"
+    t.date     "date"
     t.integer  "account_id"
     t.integer  "amount"
     t.datetime "created_at"
