b936e53930beedfd334a383e16fc63c147486ce8
jpmenniti <jpkm1989@gmail.com>
2011-03-21 20:44:07 +0300

    3

--- app/models/role.rb
+++ app/models/role.rb
@@ -16,6 +16,8 @@ class Role < ActiveRecord::Base
 		unless name.nil? || name.empty?
 			if rs.include?(name.downcase.strip)
 				errors.add_to_base('Role already used')
+			else
+				validates_format_of :name, :with => /^[-\w\._@]+$/i, :allow_blank => false, :message => "should only contain letters, numbers, or .-_@"
 			end
 		else
 			validates_presence_of :name
