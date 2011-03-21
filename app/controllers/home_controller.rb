1cfd1909e9dc4ec36e9a8fe5e2e9186b7169fdb2
jpmenniti <jpkm1989@gmail.com>
2011-03-21 10:00:08 +0300

    1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,15 @@ class HomeController < ApplicationController
   def index
 	if logged_in?
 		unless current_user.is_admin? || current_user.is_VP?
-			unless current_user.assignments.nil?
-				if current_user.assignments.count == 1
-					@club = current_user.assignments[0]
-					redirect_to club_path(@club.club_id)
+			unless current_user.assignments.nil? || current_user.assignments.empty?
+				unless current_user.assignments.count == 1 
+					for assignment in current_user.assignments
+						if assignment.active
+							redirect_to club_path(assignment.club_id)
+						end
+					end
+				else
+					redirect_to club_path(current_user.assignments[0].club_id)
 				end
 			end
 		end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,19 @@ class SessionsController < ApplicationController
     if user
 		if user.assignments.nil? || user.assignments.empty?
 			redirect_to root_url, :notice => "You don't have any assignments right now. Check yoself."
-		else
+		elsif user.assignments.count == 1 && user.assignments[0].active
 			session[:user_id] = user.id
 			redirect_to_target_or_default root_url, :notice => "Logged in successfully."
+		else
+			for assignment in user.assignments
+				unless assignment.active
+					break
+				end
+			redirect_to root_url, :notice => "You don't have any assignments right now. Check yoself."
+			end
 		end
 	else
-	 redirect_to root_url, :notice => "Invalid login or password."
+		redirect_to root_url, :notice => "Invalid login or password."
     end
   end
 
