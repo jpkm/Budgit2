475e1bb79c608449450bd4a1203856b416123b67
jpmenniti <jpkm1989@gmail.com>
2011-03-21 22:25:14 +0300

    4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,7 @@ class HomeController < ApplicationController
 	if logged_in?
 		unless current_user.is_admin? || current_user.is_vp?
 			unless current_user.assignments.nil? || current_user.assignments.empty?
-				unless current_user.assignments.count == 1 
-					for assignment in current_user.assignments
-						if assignment.active
-							redirect_to club_path(assignment.club_id)
-						end
-					end
-				else
+				if current_user.assignments.count == 1 
 					redirect_to club_path(current_user.assignments[0].club_id)
 				end
 			end
