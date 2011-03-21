a3da0a6b340d91a9b8ba156e13d7fbb37d203b91
jpmenniti <jpkm1989@gmail.com>
2011-03-21 12:01:30 +0300

    3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,13 +17,33 @@ class SessionsController < ApplicationController
 				unless assignment.active
 					break
 				end
-			redirect_to root_url, :notice => "You don't have any assignments right now. Check yoself."
+			#redirect_to root_url, :notice => "You don't have any active assignments right now. Check yoself."
 			end
+			redirect_to root_url, :notice => "You don't have any active assignments right now. Check yoself."
 		end
 	else
 		redirect_to root_url, :notice => "Invalid login or password."
     end
   end
+  
+  def available_users
+		free_users = []
+		for user in User.all
+			unless user.assignments.nil? || user.assignments.empty?
+				for assignment in user.assignments
+					if assignment.active
+						break
+					end
+				free_users << user
+				end
+			else
+				free_users << user
+			end
+		end
+		return free_users
+	end
+		
+  
 
   def destroy
     session[:user_id] = nil
